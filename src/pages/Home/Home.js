e3ca43ea7f8a3504790995431928bc845ba296a5
Isaac Angle <isaacangle@Isaacs-MacBook-Pro.local>
2022-02-16 12:56:40 -0800

    tried to use amplify to get the login to work but decided against using it

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import homeImage from '../../Images/homePageFoodImage.jpg';
+import {useHistory} from 'react-router-dom';
+import './Home.css'
 
 function Home(){
 
+  let history = useHistory();
+
+  function linkClicked(){
+    history.push('/Order');
+  }
+
   return(
-    <div>
-      Hello There!
+    <div className='mainDiv'>  
+      <button class="button" className='loginLink' onClick={linkClicked}> Login/Sign Up</button>
+      <img className='mainImage' src={homeImage} alt='Background'></img>
     </div>
+
   );
 }
 
